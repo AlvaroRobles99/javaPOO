63c44df8283152bf17d0f6479502afa72ca4a1d1
Alvaro Robles <alvaroagusrobles@gmail.com>
2025-08-06 19:21:33 -0300

    clase 2

---
+++ javaPoo1/src/Article.java
@@ -0,0 +1,15 @@
+public class Article {
+    private String nombre;
+    private Integer dosis;
+    private Float precio;
+ public Article(String nombre, Integer dosis, Float precio){
+   this.nombre = nombre;
+   this.dosis = dosis;
+   this.precio = precio;
+ }
+
+
+
+
+
+}
