8bb450cd72256db9c308acb13dd492a15a8ad2fa
Alvaro Robles <alvaroagusrobles@gmail.com>
2025-08-19 15:34:35 -0300

    clase6teoria

---
+++ java/TP2/src/clase6/INotificable.java
@@ -0,0 +1,9 @@
+package clase6;
+public interface INotificable {
+    
+public void enviar(Notificacion noti);
+    
+} 
+// TODO 1: Definir la interfaz pública INotificable.
+// TODO 2: Declarar el método abstracto del contrato, el cual debe ser // público,sin valor de retorno y aceptar un parámetro de tipo  
+// Notificacion.
